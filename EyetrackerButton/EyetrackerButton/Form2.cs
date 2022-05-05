56439f2fb5cc8b5efcb1bb53431acbffc4f6c7ed
Jonas Vannieuwenhuijsen <jonas.vannieuwenhuijsen@student.uhasselt.be>
2022-05-05 09:16:07 +0200

    callibration

--- EyetrackerButton/EyetrackerButton/Form2.cs
+++ EyetrackerButton/EyetrackerButton/Form2.cs
@@ -15,10 +15,8 @@ namespace EyetrackerButton
     {
         public static Tuple<IntPtr, IntPtr> calConnection;
 
-        private static TimerCallback timerCallback = new TimerCallback(timerCount);
         private System.Threading.Timer timer1;
         public Thread threadCallibration;
-        public static int count = 150;
 
         public Form2(Tuple<IntPtr, IntPtr> connection)
         {
@@ -28,7 +26,6 @@ namespace EyetrackerButton
 
             calConnection = connection;
 
-            threadCallibration = new Thread(new ThreadStart(Calibrate));
 
 
         }
@@ -41,14 +38,11 @@ namespace EyetrackerButton
 
             float formHeight = this.Height;
             float formWidth = this.Width;
-            count = 150;
 
             pictBoxCal1.Location = new Point((int)(formWidth * 0.5 - pictBoxCal1.Width/2), ((int)(formHeight * 0.5 - pictBoxCal1.Height/2)));
             pictBoxCal1.BackColor = Color.Red;
 
-            threadCallibration.Start();
-
-            //timer1 = new System.Threading.Timer(timerCallback, null, 250, 16);
+            timer1 = new System.Threading.Timer(new TimerCallback(Calibrate), null, 250, 25);
         }
 
       
@@ -90,138 +84,124 @@ namespace EyetrackerButton
         }
 
 
-
-        private static void timerCount(object msg)
-        {
-            count--;
-        }
-
-
         public int i = 0;
-        private void Calibrate()
+        private void Calibrate(object msg)
         {
-            while (true)
+        
+            float formHeight = this.Height;
+            float formWidth = this.Width;
+
+            i++;
+            Console.WriteLine(i);
+
+            if (pictBoxCal1.Width > 15)
             {
-                float formHeight = this.Height;
-                float formWidth = this.Width;
-
-                Console.WriteLine(count);
-
-                if (pictBoxCal1.Width > 15)
+                pictBoxCal1.Invoke(new MethodInvoker(delegate
                 {
-                    pictBoxCal1.Invoke(new MethodInvoker(delegate
-                    {
-                        pictBoxCal1.Width = count;
-                        pictBoxCal1.Height = count;
-                        pictBoxCal1.Location = new Point((int)(formWidth * 0.5 - pictBoxCal1.Width / 2), ((int)(formHeight * 0.5 - pictBoxCal1.Height / 2)));
-                    }));
+                    pictBoxCal1.Width--;
+                    pictBoxCal1.Height--;
+                    pictBoxCal1.Location = new Point((int)(formWidth * 0.5 - pictBoxCal1.Width / 2), ((int)(formHeight * 0.5 - pictBoxCal1.Height / 2)));
+                }));
 
-                    if (pictBoxCal1.Width == 15)
-                    {
-                        TobiiTracker.calcPnt(calConnection.Item1, 0.5, 0.5);
-                        pictBoxCal1.BackColor = Color.Green;
-
-                        pictBoxCal2.Invoke(new MethodInvoker(delegate
-                        {
-                            pictBoxCal2.Location = new Point((int)(formWidth * 0.025 - pictBoxCal2.Width / 2), ((int)(formHeight * 0.025 - pictBoxCal2.Height / 2)));
-                            pictBoxCal2.BackColor = Color.Red;
-                        }));
-
-                        count = 150;
-                    }
-                }
-                else if (pictBoxCal2.Width > 15)
+                if (pictBoxCal1.Width == 15)
                 {
+                    TobiiTracker.calcPnt(calConnection.Item1, 0.5, 0.5);
+                    pictBoxCal1.BackColor = Color.Green;
+
                     pictBoxCal2.Invoke(new MethodInvoker(delegate
                     {
-                        pictBoxCal2.Width = count;
-                        pictBoxCal2.Height = count;
                         pictBoxCal2.Location = new Point((int)(formWidth * 0.025 - pictBoxCal2.Width / 2), ((int)(formHeight * 0.025 - pictBoxCal2.Height / 2)));
+                        pictBoxCal2.BackColor = Color.Red;
                     }));
-                    
-                    if (pictBoxCal2.Width == 15)
-                    {
-                        TobiiTracker.calcPnt(calConnection.Item1, 0.025, 0.025);
-                        pictBoxCal2.BackColor = Color.Green;
-
-                        pictBoxCal3.Invoke(new MethodInvoker(delegate
-                        {
-                            pictBoxCal3.Location = new Point((int)(formWidth * 0.025 - pictBoxCal3.Width / 2), ((int)(formHeight * 0.975 - pictBoxCal3.Height / 2)));
-                            pictBoxCal3.BackColor = Color.Red;
-                        }));
-
-                        count = 150;
-                    }
-                }
-                else if (pictBoxCal3.Width > 15)
-                {
-                    pictBoxCal3.Invoke(new MethodInvoker(delegate
-                    {
-                        pictBoxCal3.Width = count;
-                        pictBoxCal3.Height = count;
-                        pictBoxCal3.Location = new Point((int)(formWidth * 0.025 - pictBoxCal3.Width / 2), ((int)(formHeight * 0.975 - pictBoxCal3.Height / 2)));
-                    }));
-                    
-                    if (pictBoxCal3.Width == 15)
-                    {
-                        TobiiTracker.calcPnt(calConnection.Item1, 0.025, 0.975);
-                        pictBoxCal3.BackColor = Color.Green;
-
-                        pictBoxCal4.Invoke(new MethodInvoker(delegate
-                        {
-                            pictBoxCal4.Location = new Point((int)(formWidth * 0.975 - pictBoxCal4.Width / 2), ((int)(formHeight * 0.975 - pictBoxCal4.Height / 2)));
-                            pictBoxCal4.BackColor = Color.Red;
-                        }));
-
-                        count = 150;
-                    }
-                }
-                else if (pictBoxCal4.Width > 15)
-                {
-                    pictBoxCal4.Invoke(new MethodInvoker(delegate
-                    {
-                        pictBoxCal4.Width = count;
-                        pictBoxCal4.Height = count;
-                        pictBoxCal4.Location = new Point((int)(formWidth * 0.975 - pictBoxCal4.Width / 2), ((int)(formHeight * 0.975 - pictBoxCal4.Height / 2)));
-                    }));
-                    
-                    if (pictBoxCal4.Width == 15)
-                    {
-                        TobiiTracker.calcPnt(calConnection.Item1, 0.975, 0.975);
-                        pictBoxCal4.BackColor = Color.Green;
-
-                        pictBoxCal5.Invoke(new MethodInvoker(delegate
-                        {
-                            pictBoxCal5.Location = new Point((int)(formWidth * 0.975 - pictBoxCal5.Width / 2), ((int)(formHeight * 0.025 - pictBoxCal5.Height / 2)));
-                            pictBoxCal5.BackColor = Color.Red;
-                        }));
-
-                        count = 150;
-                    }
-                }
-                else if (pictBoxCal5.Width > 15)
-                {
-                    pictBoxCal5.Invoke(new MethodInvoker(delegate
-                    {
-                        pictBoxCal5.Width = count;
-                        pictBoxCal5.Height = count;
-                        pictBoxCal5.Location = new Point((int)(formWidth * 0.975 - pictBoxCal5.Width / 2), ((int)(formHeight * 0.025 - pictBoxCal5.Height / 2)));
-                    }));
-
-                    if (pictBoxCal5.Width == 15)
-                    {
-                        TobiiTracker.calcPnt(calConnection.Item1, 0.975, 0.025);
-                        pictBoxCal5.BackColor = Color.Green;
-                    }
-                }
-                else
-                {
-                    break;
                 }
             }
+            else if (pictBoxCal2.Width > 15)
+            {
+                pictBoxCal2.Invoke(new MethodInvoker(delegate
+                {
+                    pictBoxCal2.Width--;
+                    pictBoxCal2.Height--;
+                    pictBoxCal2.Location = new Point((int)(formWidth * 0.025 - pictBoxCal2.Width / 2), ((int)(formHeight * 0.025 - pictBoxCal2.Height / 2)));
+                }));
+                    
+                if (pictBoxCal2.Width == 15)
+                {
+                    TobiiTracker.calcPnt(calConnection.Item1, 0.025, 0.025);
+                    pictBoxCal2.BackColor = Color.Green;
 
-            threadCallibration.Abort();
+                    pictBoxCal3.Invoke(new MethodInvoker(delegate
+                    {
+                        pictBoxCal3.Location = new Point((int)(formWidth * 0.025 - pictBoxCal3.Width / 2), ((int)(formHeight * 0.975 - pictBoxCal3.Height / 2)));
+                        pictBoxCal3.BackColor = Color.Red;
+                    }));
+                }
+            }
+            else if (pictBoxCal3.Width > 15)
+            {
+                pictBoxCal3.Invoke(new MethodInvoker(delegate
+                {
+                    pictBoxCal3.Width--;
+                    pictBoxCal3.Height--;
+                    pictBoxCal3.Location = new Point((int)(formWidth * 0.025 - pictBoxCal3.Width / 2), ((int)(formHeight * 0.975 - pictBoxCal3.Height / 2)));
+                }));
+                    
+                if (pictBoxCal3.Width == 15)
+                {
+                    TobiiTracker.calcPnt(calConnection.Item1, 0.025, 0.975);
+                    pictBoxCal3.BackColor = Color.Green;
 
+                    pictBoxCal4.Invoke(new MethodInvoker(delegate
+                    {
+                        pictBoxCal4.Location = new Point((int)(formWidth * 0.975 - pictBoxCal4.Width / 2), ((int)(formHeight * 0.975 - pictBoxCal4.Height / 2)));
+                        pictBoxCal4.BackColor = Color.Red;
+                    }));
+                }
+            }
+            else if (pictBoxCal4.Width > 15)
+            {
+                pictBoxCal4.Invoke(new MethodInvoker(delegate
+                {
+                    pictBoxCal4.Width--;
+                    pictBoxCal4.Height--;
+                    pictBoxCal4.Location = new Point((int)(formWidth * 0.975 - pictBoxCal4.Width / 2), ((int)(formHeight * 0.975 - pictBoxCal4.Height / 2)));
+                }));
+                    
+                if (pictBoxCal4.Width == 15)
+                {
+                    TobiiTracker.calcPnt(calConnection.Item1, 0.975, 0.975);
+                    pictBoxCal4.BackColor = Color.Green;
+
+                    pictBoxCal5.Invoke(new MethodInvoker(delegate
+                    {
+                        pictBoxCal5.Location = new Point((int)(formWidth * 0.975 - pictBoxCal5.Width / 2), ((int)(formHeight * 0.025 - pictBoxCal5.Height / 2)));
+                        pictBoxCal5.BackColor = Color.Red;
+                    }));
+                }
+            }
+            else if (pictBoxCal5.Width > 15)
+            {
+                pictBoxCal5.Invoke(new MethodInvoker(delegate
+                {
+                    pictBoxCal5.Width--;
+                    pictBoxCal5.Height--;
+                    pictBoxCal5.Location = new Point((int)(formWidth * 0.975 - pictBoxCal5.Width / 2), ((int)(formHeight * 0.025 - pictBoxCal5.Height / 2)));
+                }));
+
+                if (pictBoxCal5.Width == 15)
+                {
+                    TobiiTracker.calcPnt(calConnection.Item1, 0.975, 0.025);
+                    pictBoxCal5.BackColor = Color.Green;
+                }
+            }
+            else
+            {
+                timer1.Change(Timeout.Infinite, Timeout.Infinite);
+                TobiiTracker.computeApplyStopPersonCal(calConnection.Item1);
+                this.Invoke((MethodInvoker)delegate
+                {
+                    this.Close();
+                });
+            }
         }
     }
 }
